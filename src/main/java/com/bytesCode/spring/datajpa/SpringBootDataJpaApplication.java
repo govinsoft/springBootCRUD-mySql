0bcc8f0e0d48d5822cd0fd31374851d8678c2558
govin <govinsoft@gmail.com>
2022-09-27 14:35:10 +0530

    future....1 changes

--- src/main/java/com/bytesCode/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/bytesCode/spring/datajpa/SpringBootDataJpaApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootDataJpaApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(SpringBootDataJpaApplication.class, args);
+
+		System.out.println("Future......1");
 	}
 
 }
