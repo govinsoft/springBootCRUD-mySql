18af7cfc7332f0f3ebdf8bdde065f78ccb75347f
govin <govinsoft@gmail.com>
2022-11-03 17:26:57 +0530

    jUnit implementation

---
+++ src/main/java/com/bytesCode/spring/datajpa/advice/LoggingAdvice.java
@@ -0,0 +1,47 @@
+
+package com.bytecodes.spring.aop.api.advice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAdvice {
+
+	Logger log = LoggerFactory.getLogger(LoggingAdvice.class);
+	
+	@Pointcut(value="execution(* com.bytecodes.spring.aop.api.*.*.*(..) )")
+	public void myPointcut() {
+		
+	}
+	
+	@Around("myPointcut()")
+	public Object applicationLogger(ProceedingJoinPoint pjp) throws Throwable {
+
+ ObjectMapper mapper = new ObjectMapper();
+		String methodName = pjp.getSignature().getName();
+		String className = pjp.getTarget().getClass().toString();
+		Object[] array = pjp.getArgs();
+		log.info("method invoked " + className + " : " + methodName + "()" + "arguments : "
+				+ mapper.writeValueAsString(array));
+		Object object = pjp.proceed();
+		log.info(className + " : " + methodName + "()" + "Response : "
+				+ mapper.writeValueAsString(object));
+		return object;
+
+
+
+
+
+
+
+	}
+
+}
+
